6838f54e58fbbbef54ce8ef9af89b53b0882b262
Shubham Gupta <112654423+MrShubhG1@users.noreply.github.com>
2026-05-28 16:38:59 +0530

    Update MainApplication.kt

--- android/app/src/main/java/com/tempapp/MainApplication.kt
+++ android/app/src/main/java/com/tempapp/MainApplication.kt
@@ -1,27 +1,30 @@
 package com.tempapp
 
 import android.app.Application
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultReactNativeHost
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactHost: ReactHost by lazy {
-    getDefaultReactHost(
-      context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
-    )
-  }
+    private val mReactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
 
-  override fun onCreate() {
-    super.onCreate()
-    loadReactNative(this)
-  }
+            override fun getPackages(): List<ReactPackage> {
+                return emptyList()
+            }
+
+            override fun getUseDeveloperSupport(): Boolean {
+                return true
+            }
+
+            override fun getJSMainModuleName(): String {
+                return "index"
+            }
+        }
+
+    override fun getReactNativeHost(): ReactNativeHost {
+        return mReactNativeHost
+    }
 }
