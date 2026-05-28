57b0879dcc10da91829a38febc468a6ad7517212
Shubham Gupta <112654423+MrShubhG1@users.noreply.github.com>
2026-05-28 16:48:50 +0530

    Update MainApplication.kt

--- android/app/src/main/java/com/tempapp/MainApplication.kt
+++ android/app/src/main/java/com/tempapp/MainApplication.kt
@@ -3,12 +3,17 @@ package com.tempapp
 import android.app.Application
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactNativeHost
 
 class MainApplication : Application(), ReactApplication {
 
     override val reactNativeHost: ReactNativeHost =
         object : DefaultReactNativeHost(this) {
+
+            override fun getPackages(): List<ReactPackage> {
+                return mutableListOf()
+            }
 
             override fun getUseDeveloperSupport(): Boolean {
                 return true
